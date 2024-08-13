f824796faf45d5091f6c3fa9fe2a8413d4e2a073
thezeeshann <info.zeeshann@gmail.com>
2024-08-13 23:42:31 +0530

    refactor: update home

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./common/Navbar";
-import Footer from "./common/Footer"
-import Error from "./pages/Error"
+import Home from "./components/Home";
+import Error from "./components/Error";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <main className="flex flex-col mx-auto bg-darkBgPrimary w-full h-full font-Poppins">
-        <Navbar />
+      <main className="flex flex-col w-full h-full mx-auto bg-darkBgPrimary font-Poppins">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Error/>} />
+          <Route path="*" element={<Error />} />
         </Routes>
-        <Footer/>
       </main>
     </>
   );
