6d59ad2f21b9fb8be5c212027e6060dee96e1a53
thezeeshann <info.zeeshann@gmail.com>
2024-09-22 11:30:30 +0530

    update constants

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,8 +1,57 @@
 export const userMetaData = {
-  bio: "focused on creating innovative and modern web and mobile apps. With a passion for continuous improvement, I thrive on bringing creative ideas to life through technology.",
+  bio: "focused on creating innovative and modern web apps. With a passion for continuous improvement, I thrive on bringing creative ideas to life through technology.",
 };
 
 export const projects = [
+  {
+    id: 2,
+    name: "Testimonial",
+    description:
+      "Get testimonials from your customers with ease.",
+    techstacks: [
+      {
+        id: 1,
+        name: "Nextjs",
+      },
+      {
+        id: 2,
+        name: "TypeScript",
+      },
+      {
+        id: 3,
+        name: "Tailwind CSS",
+      },
+      {
+        id: 4,
+        name: "Shadcn UI",
+      },
+      {
+        id: 5,
+        name: "React Query",
+      },
+      {
+        id: 6,
+        name: "PostgreSQL",
+      },
+      {
+        id: 7,
+        name: "Next Auth",
+      },
+      {
+        id: 8,
+        name: "Prisma",
+      },
+      {
+        id: 9,
+        name: "Zod",
+      },
+      {
+        id: 10,
+        name: "Uploadthing",
+      },
+    ],
+    github: "https://github.com/thezeeshann/testimonial",
+  },
   {
     id: 1,
     name: "Ecommerce Application",
